864620b663c4d09c9c481dfd6dccbf65ca79f6af
Noah Ryu Nguyen <noah@ryu.dk>
2024-06-06 15:15:47 +0200

    Pulled CGD folder from AA_testing branch

---
+++ CGD/AA_model.py
@@ -0,0 +1,45 @@
+import torch
+from time import time
+
+class AA(torch.nn.Module):
+    def __init__(self, num_comp, X, model="AA", init=None, verbose=False):
+        super().__init__()
+        if verbose:
+            print('Initializing model: ' + model)
+            t1 = time()
+
+        self.model = model
+
+        K = num_comp
+        P = X.shape[-1]
+        self.X = X
+
+        self.softmaxG = torch.nn.Softmax(dim=0)
+        self.softmaxS = torch.nn.Softmax(dim=-2)
+
+        if init is None:
+            self.G = torch.nn.Parameter(self.softmaxG(-torch.log(torch.rand((P, K), dtype=torch.double))))
+            self.S = torch.nn.Parameter(self.softmaxS(-torch.log(torch.rand([K, P], dtype=torch.double))))
+        else:
+            self.G = init['G'].clone()
+            self.S = init['S'].clone()
+
+        if verbose:
+            t2 = time()
+            print('Model initialized in ' + str(t2 - t1) + ' seconds')
+
+    def get_model_params(self):
+        with torch.no_grad():
+            S_soft = self.softmaxS(self.S)
+            G_soft = self.softmaxG(self.G)
+            return G_soft.detach(), S_soft.detach()
+
+    def forward(self):
+        S_soft = self.softmaxS(self.S)
+        G_soft = self.softmaxG(self.G)
+
+        loss = torch.sum(torch.linalg.matrix_norm(self.X - self.X @ G_soft @ S_soft)**2)
+
+        if torch.isnan(loss):
+            raise ValueError('Loss is NaN')
+        return loss
