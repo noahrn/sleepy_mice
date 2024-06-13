02b745d7b652d297aae579762271fcfe184f3aeb
AugustBorgLjorring <august.ljorring.skole@gmail.com>
2024-06-13 13:50:34 +0200

    Added classweights and noise_term

--- src/results.py
+++ src/results.py
@@ -6,7 +6,7 @@ from tqdm import tqdm
 from CGD import AA_model, AA_trainer
 
 # Load and process data
-df = load_and_process_data(normalize=False, lab="all")
+df = load_and_process_data(normalize=False, lab="1")
 df['logrms'] = np.log1p(df['rms'])
 df.insert(7, 'logrms', df.pop('logrms'))
 
@@ -26,15 +26,26 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 data = torch.tensor(X).to(device)
 
 # Fit the model for archetypes K
-K_list = [2, 3, 4, 5, 6, 7, 8, 9, 10]
+K_list = [4]
 
 C_list_list = [[] for _ in range(len(K_list))]
 S_list_list = [[] for _ in range(len(K_list))]
 
+class_labels = torch.tensor(y, dtype=torch.long).to(device)
+unique_labels = torch.unique(class_labels)
+
+# Calculate class weights using PyTorch operations
+class_weights = torch.tensor([1 / torch.sum(class_labels == i).item() for i in unique_labels]).to(device)
+class_weights = class_weights / torch.sum(class_weights)
+
+# Expand class_weights to match each sample
+sample_weights = class_weights[class_labels - 1]
+
+
 for i, K in tqdm(enumerate(K_list)):
     # Run each model 5 times
     for _ in range(5):
-        model = AA_model.AA(X=data,num_comp=K,model='AA', verbose=False)
+        model = AA_model.AA(X=data, num_comp=K, class_weights=sample_weights, noise_term=True, model='AA', verbose=False)
         optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
         loss,_ = AA_trainer.Optimizationloop(model=model,optimizer=optimizer,max_iter=10000,tol=1e-6, disable_output=True)
         C, S = model.get_model_params()
