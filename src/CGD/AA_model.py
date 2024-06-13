Changes to use normalized weighting on features and correct weighting of RSS

src/CGD/AA_model.py
@@ -2,7 +2,7 @@ import torch
 from time import time
 
 class AA(torch.nn.Module):
-    def __init__(self, num_comp, X, model="AA", init=None, verbose=False):
+    def __init__(self, num_comp, X, class_weights=None, model="AA", init=None, verbose=False):
         super().__init__()
         if verbose:
             print('Initializing model: ' + model)
@@ -13,6 +13,12 @@ class AA(torch.nn.Module):
         K = num_comp
         P = X.shape[-1]
         self.X = X
+        self.class_weights = class_weights
+
+        # Calculate class weights if not provided
+        if class_weights is None:
+            class_weights = torch.ones(P, dtype=torch.double)
+        self.class_weights = class_weights
 
         self.softmaxG = torch.nn.Softmax(dim=0)
         self.softmaxS = torch.nn.Softmax(dim=-2)
@@ -42,8 +48,18 @@ class AA(torch.nn.Module):
         S_soft = self.softmaxS(self.S)
         G_soft = self.softmaxG(self.G)
 
-        loss = torch.sum(torch.linalg.matrix_norm(self.X - self.X @ G_soft @ S_soft)**2)
+        device = self.G.device
+        self.class_weights = self.class_weights.to(device)
 
-        if torch.isnan(loss):
+        residual = self.X - self.X @ G_soft @ S_soft
+        #residual_squared = torch.linalg.norm(residual, ord='fro') ** 2
+        #weighted_loss = torch.sum(residual_squared)
+
+        residual_squared =  torch.sum(torch.sum((residual**2) * self.class_weights, dim=1))
+        residual_normalized = torch.sum(torch.log(torch.sum((residual**2) * self.class_weights, dim=1)))
+        
+
+        if torch.isnan(residual_normalized):
             raise ValueError('Loss is NaN')
-        return loss
+        
+        return residual_normalized

src/preprocessing/data_loader.py
@@ -5,7 +5,7 @@ import json
 from sklearn.preprocessing import StandardScaler
 
 # Seeding
-np.random.seed(0)
+#np.random.seed(0)
 
 # EEG features
 eeg_features = ['slowdelta', 'fastdelta', 'slowtheta', 'fasttheta', 'alpha', 'beta', 'rms']
@@ -53,25 +53,26 @@ def load_and_process_data(normalize=True, data_path=None, lab="all", verbose=Tru
             else:
                 print(f"Normalizing data for lab {lab}...")
                 
-        scaler = StandardScaler()
+        # identify data points that are more than 3 stdv away from the mean for each feature
+        all_outliers = []
         for mouse in data['unique_id'].unique():
             mouse_data = data[data['unique_id'] == mouse]
-            for feature in eeg_features:
-                data.loc[mouse_data.index, feature] = scaler.fit_transform(mouse_data[[feature]])
-        df_standardized_3std = data.copy()
-        
-        # remove data points that are more than 3 stdv away from the mean for each EEG feature
-        all_outliers = []
-        for mouse in df_standardized_3std['unique_id'].unique():
-            mouse_data = df_standardized_3std[df_standardized_3std['unique_id'] == mouse]
             for feature in eeg_features:
                 is_within_3_std = np.abs(mouse_data[feature] - mouse_data[feature].mean()) <= 3 * mouse_data[feature].std()
                 outliers = mouse_data[~is_within_3_std].index
                 all_outliers.extend(outliers)
 
-        
-        # Drop rows with NaN values after outlier removal
-        df_standardized_3std.drop(all_outliers, inplace=True)
+        # remove all observations that have outliers
+        data.drop(all_outliers, inplace=True)
+
+        # standardize for each mouse for each feature independently
+        scaler = StandardScaler()
+        for mouse in data['unique_id'].unique():
+            mouse_data = data[data['unique_id'] == mouse]
+            for feature in eeg_features:
+                data.loc[mouse_data.index, feature] = scaler.fit_transform(mouse_data[[feature]])
+
+        df_standardized_3std = data.copy()
 
         if verbose:
             if lab == 'all':